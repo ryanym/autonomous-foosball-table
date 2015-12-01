bf72bdeab8c900b183a767db854bfd6ac9e92422
Chenhe Li <lic54@mcmaster.ca>
2015-11-30 22:17:53 -0500

    add unknown detect
    color of the edge still need to specify. improve foosmen detection
    method.

--- Tracking/ball_tracking.py
+++ Tracking/ball_tracking.py
@@ -13,7 +13,7 @@ import time
 CAM_ID = 1
 CAM_WIDTH = 320
 CAM_HEIGHT = 240
-CAM_FPS = 60
+CAM_FPS = 90
 
 '''
 The Edge of the playfield, use for cut capture image
@@ -21,10 +21,11 @@ new coordinte after cut
 0,0                         0,EDGE_Y_MAX-EDGE_Y_MIN-1
 EDGE_X_MAX-EDGE_X_MIN-1,0     EDGE_X_MAX-EDGE_X_MIN-1,EDGE_Y_MAX-EDGE_Y_MIN-1
 '''
-EDGE_X_MIN = 24
-EDGE_X_MAX = 300
-EDGE_Y_MIN = 22
-EDGE_Y_MAX = 216
+
+EDGE_X_MIN = 20
+EDGE_X_MAX = 304
+EDGE_Y_MIN = 18
+EDGE_Y_MAX = 220
 
 FIELD_L = 410 # mm
 FIELD_W = 290 # mm
@@ -42,7 +43,7 @@ ROW_3_PIXEL = FIELD_L_PIXEL*ROW_3/FIELD_L
 ROW_4_PIXEL = FIELD_L_PIXEL*ROW_4/FIELD_L
 ROW_TOL_PIXEL = FIELD_L_PIXEL*ROW_TOL/FIELD_L
 
-FOOSMAN_DISTANCE =95 #mm
+FOOSMAN_DISTANCE = 88 #mm
 FOOSMAN_WIDTH =28 #mm
 FOOSMAN_DISTANCE_PIXEL = FIELD_L_PIXEL*FOOSMAN_DISTANCE/FIELD_L
 FOOSMAN_WIDTH_PIXEL = FIELD_L_PIXEL*FOOSMAN_WIDTH/FIELD_L
@@ -71,8 +72,18 @@ UPPER_H = 116
 UPPER_S = 183
 UPPER_V = 236
 
+
+ROB_LOWHSV = np.array([0,40,159])
+ROB_UPPHSV = np.array([24,235,255])
+USER_LOWHSV = np.array([0,0,235])
+USER_UPPHSV = np.array([179,32,255])
+EDGE_LOWHSV = np.array([50,2,0])
+EDGE_UPPHSV = np.array([179,255,255])
+
+	
 ERROR_FILTER = np.ones((2,2),np.uint8)
-ERROR_FILTER_MEN = np.ones((3,3),np.uint8)
+ERROR_FILTER_MEN = np.ones((2,2),np.uint8)
+ERROR_FILTER_EDGE = np.ones((4,4),np.uint8)
 def nothing(x):
     pass
 
@@ -130,18 +141,24 @@ def find_ball_path(ball_cur,ball_pre):
 def center_ftoi(center):
     return (int(center[0]),int(center[1]))
 
+
+
 def foosmen_location(foosmen):
+    #input array of (x,y_center,w,h)
     foosmen_relocate = []
     if len(foosmen) >= 3:
-        foosmen.sort(key=lambda tup: tup[2])
-        tmp_middle = (foosmen[0][2]+foosmen[-1][2])/2
+        foosmen.sort(key=lambda tup: tup[2]*tup[3])
+        foosmen = foosmen[0:3]
+        foosmen.sort(key=lambda tup: tup[1])
+        tmp_middle = (foosmen[0][1]+foosmen[2][1])/2
         foosmen_middle = ();
-        foosmen_middle = min(foosmen, key=lambda foosman:abs(foosman[2]-tmp_middle))
+        foosmen_middle = min(foosmen, key=lambda foosman:abs(foosman[1]-tmp_middle))
         for i in (-1,0,1):
             foosmen_relocate.append( (foosmen_middle[0],
-                                      foosmen_middle[1],
-                                      foosmen_middle[2]-FOOSMAN_WIDTH_PIXEL/2+i*FOOSMAN_DISTANCE_PIXEL,
-                                      foosmen_middle[2]+FOOSMAN_WIDTH_PIXEL/2+i*FOOSMAN_DISTANCE_PIXEL) )
+                                      foosmen_middle[0]+foosmen_middle[2],
+                                      foosmen_middle[1]-FOOSMAN_WIDTH_PIXEL/2+i*FOOSMAN_DISTANCE_PIXEL,
+                                      foosmen_middle[1]+FOOSMAN_WIDTH_PIXEL/2+i*FOOSMAN_DISTANCE_PIXEL) )
+    '''
     if len(foosmen)==2:
         foosmen.sort(key=lambda tup: tup[2])
         foosmen_middle = ()
@@ -155,8 +172,66 @@ def foosmen_location(foosmen):
                                       foosmen_middle[1],
                                       foosmen_middle[2]-FOOSMAN_WIDTH_PIXEL/2+i*FOOSMAN_DISTANCE_PIXEL,
                                       foosmen_middle[2]+FOOSMAN_WIDTH_PIXEL/2+i*FOOSMAN_DISTANCE_PIXEL) )
+    '''
     return foosmen_relocate
     
+
+def unknown_detect(hsv):
+    
+    #0,1 compare with x-axis; 2,3 compare with y-axis
+    #return true when unknown detected, else false
+    hsv_edge = (hsv[0:EDGE_Y_MIN,0:CAM_WIDTH],hsv[EDGE_Y_MAX:CAM_HEIGHT,0:CAM_WIDTH],hsv[0:CAM_HEIGHT,0:EDGE_X_MIN],hsv[0:CAM_HEIGHT,EDGE_X_MAX:CAM_WIDTH])
+            
+    for edge_index in [0]:
+            edge_fil_img = cv2.inRange(hsv_edge[edge_index], EDGE_LOWHSV, EDGE_UPPHSV) 
+            dilate = cv2.dilate(edge_fil_img,ERROR_FILTER_EDGE,1)
+            asfdas,contours,hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            c = sorted(contours, key = cv2.contourArea, reverse = True)
+            edge_cover = {'x':0,'w':0}#y,h
+            for cnt in c:
+                x,y,w,h = cv2.boundingRect(cnt)
+                '''
+                if(w>edge_cover['w']):
+                    edge_cover['w'] = w
+                    edge_cover['x'] = x
+                '''
+                if  x < EDGE_X_MIN and x+w > EDGE_X_MAX:
+                    edge_cover['w'] = w
+                    edge_cover['x'] = x
+                    break
+
+            print edge_cover
+            cv2.imshow("edge"+str(edge_index),edge_fil_img) 
+            if  edge_cover['x']> EDGE_X_MIN or edge_cover['x']+edge_cover['w']< EDGE_X_MAX:
+                return True
+            
+                
+    for edge_index in [3]:
+            edge_fil_img = cv2.inRange(hsv_edge[edge_index], EDGE_LOWHSV, EDGE_UPPHSV)
+            dilate = cv2.dilate(edge_fil_img,ERROR_FILTER_EDGE,1)
+            asfdas,contours,hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            c = sorted(contours, key = cv2.contourArea, reverse = True)
+            edge_cover = {'y':0,'h':0}#y,h
+            for cnt in c:
+                x,y,w,h = cv2.boundingRect(cnt)
+                '''
+                if(h>edge_cover['h']):
+                    edge_cover['h'] = h
+                    edge_cover['y'] = y
+
+                '''
+                if  y < EDGE_Y_MIN and y+h > EDGE_Y_MAX:
+                    edge_cover['h'] = h
+                    edge_cover['y'] = y
+                    break
+            print edge_cover
+            cv2.imshow("edge"+str(edge_index),edge_fil_img)
+            if  edge_cover['y']> EDGE_Y_MIN or edge_cover['y']+edge_cover['h']< EDGE_Y_MAX:
+                print edge_cover
+                return True
+            
+            
+    return False
 
 #set up campture
 cap = cv2.VideoCapture(CAM_ID)
@@ -168,15 +243,31 @@ lowerb = np.array([LOWER_H,LOWER_S,LOWER_V])
 upperb = np.array([UPPER_H,UPPER_S,UPPER_V])
 kernel = np.ones((7,7),np.float32)/49
 
+kernel_edge = np.ones((3,3),np.float32)/9
+
 ball_pre = (-1,-1)
 ball_cur = (-1,-1)
 
 while(True):
 
     ret, frame = cap.read()
+    dst = cv2.filter2D(frame,-1,kernel)
     frame_field = frame[EDGE_Y_MIN:EDGE_Y_MAX,EDGE_X_MIN:EDGE_X_MAX]
-    dst = cv2.filter2D(frame_field,-1,kernel)
     hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
+    '''
+    hsv_edge = [None,None,None,None]
+    hsv_edge[0] = hsv[0:EDGE_Y_MIN,0:CAM_WIDTH]
+    hsv_edge[1] = hsv[EDGE_Y_MAX:CAM_HEIGHT,0:CAM_WIDTH]
+    hsv_edge[2] = hsv[0:CAM_HEIGHT,0:EDGE_X_MIN]
+    hsv_edge[3] = hsv[0:CAM_HEIGHT,EDGE_X_MAX:CAM_WIDTH]
+    '''
+    unkown = unknown_detect(hsv)
+    if(unkown):
+        print 'unkown detected'
+            
+        
+    
+    hsv = hsv[EDGE_Y_MIN:EDGE_Y_MAX,EDGE_X_MIN:EDGE_X_MAX]
     dest_image = cv2.inRange(hsv, lowerb, upperb)
     #reduce noise
     erosion = cv2.erode(dest_image,ERROR_FILTER,1)
@@ -189,7 +280,7 @@ while(True):
     cv2.line(frame_field,(ROW_3_PIXEL,0),(ROW_3_PIXEL,FIELD_W_PIXEL),(0,0,255),2,8,0)
     cv2.line(frame_field,(ROW_4_PIXEL,0),(ROW_4_PIXEL,FIELD_W_PIXEL),(0,255,0),2,8,0)
     
-    cv2.imshow("Binary Image",dilate)
+    cv2.imshow("Ball",dilate)
 
     a,contours,hierarchy = cv2.findContours(dilate, 1, 2)
     ball_found = False
@@ -220,10 +311,7 @@ while(True):
     else:
         print 'undetected'
         
-    ROB_LOWHSV = np.array([0,99,129])
-    ROB_UPPHSV = np.array([23,235,255])
-    USER_LOWHSV = np.array([0,0,225])
-    USER_UPPHSV = np.array([179,20,255])
+
 
     first_fil_img = cv2.inRange(hsv, USER_LOWHSV, USER_UPPHSV)
     erosion = cv2.erode(first_fil_img,ERROR_FILTER_MEN,1)
@@ -237,12 +325,12 @@ while(True):
     row4=[]
     for cnt in c:
             x,y,w,h = cv2.boundingRect(cnt)
-            if (h < 30)and(w>3)and (h>3):    
+            if (h < 30)and(w>6)and (h>6):    
                 if x+w+ROW_TOL_PIXEL > ROW_2_PIXEL and x-ROW_TOL_PIXEL < ROW_2_PIXEL:
-                    row2.append((x,w+x,y+h/2))
+                    row2.append((x,y+h/2,w,h))
                     #cv2.rectangle(frame_field,(x,y),(x+w,y+h),(0,255,0),2)
                 if x+w+ROW_TOL_PIXEL > ROW_4_PIXEL and x-ROW_TOL_PIXEL < ROW_4_PIXEL:
-                    row4.append((x,w+x,y+h/2))
+                    row4.append((x,y+h/2,w,h))
                     #cv2.rectangle(frame_field,(x,y),(x+w,y+h),(0,255,0),2)
     first_fil_img = cv2.inRange(hsv, ROB_LOWHSV, ROB_UPPHSV)
     erosion = cv2.erode(first_fil_img,ERROR_FILTER_MEN,1)
@@ -252,17 +340,18 @@ while(True):
     c = sorted(contours, key = cv2.contourArea, reverse = True)
     for cnt in c:
             x,y,w,h = cv2.boundingRect(cnt)
-            if (h < 30)and(w>3)and (h>3):
+            if (h < 30)and(w>4)and (h>5):
                 if x+w+ROW_TOL_PIXEL > ROW_1_PIXEL and x-ROW_TOL_PIXEL < ROW_1_PIXEL:
-                    row1.append((x,w+x,y+h/2))
+                    row1.append((x,y+h/2,w,h))
                     #cv2.rectangle(frame_field,(x,y),(x+w,y+h),(0,0,255),2)
                 if x+w+ROW_TOL_PIXEL > ROW_3_PIXEL and x-ROW_TOL_PIXEL < ROW_3_PIXEL:
-                    row3.append((x,w+x,y+h/2))
+                    row3.append((x,y+h/2,w,h))
                     #cv2.rectangle(frame_field,(x,y),(x+w,y+h),(0,0,255),2)
 
     row1_foosmen = foosmen_location(row1)
     for i in row1_foosmen:
         cv2.rectangle(frame_field,(i[0],i[2]),(i[1],i[3]),(0,0,255),2)
+    
     row3_foosmen = foosmen_location(row3)
     for i in row3_foosmen:
         cv2.rectangle(frame_field,(i[0],i[2]),(i[1],i[3]),(0,0,255),2)
