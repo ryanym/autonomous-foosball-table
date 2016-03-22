d4830abf3290c016969e1b0d78b9ebc101fd5c3e
tahahussain4 <hussat@mcmaster.ca>
2016-03-22 14:59:46 -0400

    safety feature in comm
    added the feature that recognizes t or f flag for safety and swithces
    motors off accordingly

--- Motor/ReadSteps.ino
+++ Motor/ReadSteps.ino
@@ -2,21 +2,26 @@ void ReadSteps(float* lengths_angles,bool* safety){
   time_start  = micros();
   String content = "";
 
-  //Read input strings
+  //Read input strings and raise Read falg
   while(Serial.available() > 0 ){
     content = Serial.readStringUntil('n');
     Read = 1;  
     serial_read = true; 
   }
 
- 
+ //if flag riased dissect into propeer array
   if(Read == 1){
     for(int i = 0; i < 4; i++){
       int index = content.indexOf(","); 
       lengths_angles[i] = atol(content.substring(0,index).c_str()); 
       content = content.substring(index+1); 
     }
-    //  need to assign safety bit to something here rym
+
+    //check safety flag
+    //int index = content.indexOf(","); 
+    //if(content.substring(0,index)[0] == 't'){
+      //safety_broken();
+      //}
   
     time_end = micros();
     time_elapsed = time_end - time_start;
@@ -26,6 +31,7 @@ void ReadSteps(float* lengths_angles,bool* safety){
     Serial.println(lengths_angles[1]);
     Serial.println(lengths_angles[2]);
     Serial.println(lengths_angles[3]);
+    
 
     Read = 0;
   }
